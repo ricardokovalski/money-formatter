06481068c879270d32d3495e84bcc0bb4cacf0a2
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2021-05-06 14:34:11 -0300

    Implementando testes para as exceptions.

--- tests/BaseFormatterConfigTest.php
+++ tests/BaseFormatterConfigTest.php
@@ -5,24 +5,45 @@ namespace RicardoKovalski\CurrencyFormatter\Tests;
 use PHPUnit\Framework\TestCase;
 use RicardoKovalski\CurrencyFormatter\Enums\IsoCodes;
 use RicardoKovalski\CurrencyFormatter\Enums\Locale;
+use RicardoKovalski\CurrencyFormatter\Exceptions\CurrencyIsoCodeException;
+use RicardoKovalski\CurrencyFormatter\Exceptions\LocaleException;
 use RicardoKovalski\CurrencyFormatter\Formatters\BaseFormatterConfig;
 
 class BaseFormatterConfigTest extends TestCase
 {
     private $formatterConfig;
 
-    public function setUp()
+    public function testExpectedExceptionWhenCurrencyIsoCodeIsNotStringOnConstruct()
     {
-        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
+        $this->expectException(CurrencyIsoCodeException::class);
+        $this->formatterConfig = new BaseFormatterConfig(1, Locale::PT_BR);
+    }
+    public function testExpectedExceptionWhenCurrencyIsoCodeIsEmptyStringOnConstruct()
+    {
+        $this->expectException(CurrencyIsoCodeException::class);
+        $this->formatterConfig = new BaseFormatterConfig("", Locale::PT_BR);
+    }
+
+    public function testExpectedExceptionWhenLocaleIsNotStringOnConstruct()
+    {
+        $this->expectException(LocaleException::class);
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, 1);
+    }
+    public function testExpectedExceptionWhenLocaleIsEmptyStringOnConstruct()
+    {
+        $this->expectException(LocaleException::class);
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, "");
     }
 
     public function testAssertEqualsLocale()
     {
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
         $this->assertEquals(Locale::PT_BR, $this->formatterConfig->getLocale());
     }
 
     public function testAssertEqualsResetLocale()
     {
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
         $this->formatterConfig->resetLocale(Locale::EN_US);
 
         $this->assertEquals(Locale::EN_US, $this->formatterConfig->getLocale());
@@ -30,11 +51,13 @@ class BaseFormatterConfigTest extends TestCase
 
     public function testAssertEqualsCurrencyIsoCode()
     {
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
         $this->assertEquals(IsoCodes::BRL, $this->formatterConfig->getCurrencyIsoCode());
     }
 
     public function testAssertEqualsResetCurrencyIsoCode()
     {
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
         $this->formatterConfig->resetCurrencyIsoCode(IsoCodes::USD);
 
         $this->assertEquals(IsoCodes::USD, $this->formatterConfig->getCurrencyIsoCode());
@@ -42,6 +65,7 @@ class BaseFormatterConfigTest extends TestCase
 
     public function testAssertEqualsStyleIntl()
     {
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
         $this->assertEquals(null, $this->formatterConfig->getStyleIntl());
 
         $this->formatterConfig->resetStyleIntl(\NumberFormatter::CURRENCY);
@@ -51,6 +75,7 @@ class BaseFormatterConfigTest extends TestCase
 
     public function testAssertEqualsResetStyleIntl()
     {
+        $this->formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
         $this->formatterConfig->resetStyleIntl(\NumberFormatter::DECIMAL);
 
         $this->assertEquals(\NumberFormatter::DECIMAL, $this->formatterConfig->getStyleIntl());
