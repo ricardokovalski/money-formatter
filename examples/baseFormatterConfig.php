67c605f00529d8aa6afb081f18534e329c7b76f8
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2021-05-25 19:57:22 -0300

    Adicionando novo exemplo baseFormatterConfig.

---
+++ examples/baseFormatterConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '../vendor/autoload.php';
+
+use RicardoKovalski\CurrencyFormatter\Enums\IsoCodes;
+use RicardoKovalski\CurrencyFormatter\Enums\Locale;
+use RicardoKovalski\CurrencyFormatter\Formatters\BaseFormatterConfig;
+
+$formatterConfig = new BaseFormatterConfig(IsoCodes::BRL, Locale::PT_BR);
+
+$formatterConfig->resetCurrencyIsoCode(IsoCodes::USD);
+$formatterConfig->resetLocale(Locale::EN_US);
